1d525cbab84094fe7903d27a5e99a041153d9d75
hawoon12 <hayoong12@naver.com>
2021-05-01 20:59:50 +0900

    feat : Car class 정의

---
+++ src/main/java/car/Car.java
@@ -0,0 +1,42 @@
+package car;
+
+public class Car {
+  private String name;
+  private int position;
+
+  @Override
+  public String toString() {
+    return "Car{" +
+        "name='" + name + '\'' +
+        ", position=" + position +
+        '}';
+  }
+
+  public Car() {
+  }
+
+  public Car(String name) {
+    this.name = name;
+  }
+
+  public Car(String name, int position) {
+    this.name = name;
+    this.position = position;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getPosition() {
+    return position;
+  }
+
+  public void setPosition(int position) {
+    this.position = position;
+  }
+}
